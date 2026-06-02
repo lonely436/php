afa626817b5af0bd6c8e94f03cf0a7c96bc3d40c
lonely436 <2379223656@qq.com>
2026-06-02 14:49:09 +0800

    修复：添加 jQuery 和 Bootstrap JS 支持下拉菜单

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -15,6 +15,8 @@
 
 <body>
     <div id="app"></div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@3.3.13/dist/vue.global.prod.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue-router@4.2.5/dist/vue-router.global.prod.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios@1.6.2/dist/axios.min.js"></script>
